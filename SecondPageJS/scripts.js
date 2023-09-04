97333a44823b2b35e47128da7be5df04aeb44df7
ufjanjua <143862471+ufjanjua@users.noreply.github.com>
2023-09-04 01:13:00 -0500

    Add files via upload

---
+++ SecondPageJS/scripts.js
@@ -0,0 +1,59 @@
+function ResourceDropdown()
+{
+  document.getElementById("resources").classList.toggle("show");
+}
+function ClassesDropdown()
+{
+  document.getElementById("classes").classList.toggle("show");
+}
+function MessagesDropdown()
+{
+  document.getElementById("messages").classList.toggle("show");
+}
+function DiscussionsDropdown()
+{
+  document.getElementById("discussionsdropdown").classList.toggle("show");
+}
+function NotificationsDropdown()
+{
+  document.getElementById("notificationsdropdown").classList.toggle("show");
+}
+function UserDropdown()
+{
+  document.getElementById("userdropdown").classList.toggle("show");
+}
+function SettingsDropdown()
+{
+  document.getElementById("settingsdropdown").classList.toggle("show");
+}
+function HideAnnouncements()
+{
+  document.getElementById("announcements").classList.toggle("noshow");
+  document.getElementById("HideAnnouncements").classList.toggle("show");
+}
+function HideCourses()
+{
+  document.getElementById("clist").classList.toggle("noshow");
+  document.getElementById("HideCourses").classList.toggle("show");
+}
+function HideSupport()
+{
+  document.getElementById("D2LSupport").classList.toggle("noshow");
+  document.getElementById("HideSupport").classList.toggle("show");
+
+}
+function HideCalendar()
+{
+  document.getElementById("CalendarAppear").classList.toggle("show");
+  document.getElementById("calendar").classList.toggle("noshow");
+}
+function HideDate()
+{
+  document.getElementById("DissappearContent1").classList.toggle("show");
+  document.getElementById("AppearContent1").classList.toggle("noshow");
+}
+function HideEvents()
+{
+  document.getElementById("DissappearContent3").classList.toggle("show");
+  document.getElementById("AppearContent2").classList.toggle("noshow");
+}
---
+++ ThirdPageJS/scripts.js
@@ -0,0 +1,39 @@
+function ClassesDropdown()
+{
+  document.getElementById("classes").classList.toggle("show");
+}
+function MessagesDropdown()
+{
+  document.getElementById("messages").classList.toggle("show");
+}
+function DiscussionsDropdown()
+{
+  document.getElementById("discussionsdropdown").classList.toggle("show");
+}
+function NotificationsDropdown()
+{
+  document.getElementById("notificationsdropdown").classList.toggle("show");
+}
+function UserDropdown()
+{
+  document.getElementById("userdropdown").classList.toggle("show");
+}
+function SettingsDropdown()
+{
+  document.getElementById("settingsdropdown").classList.toggle("show");
+}
+function HiddenAnnouncements()
+{
+  document.getElementById("CourseAnnouncements").classList.toggle("noshow");
+  document.getElementById("HideAnnouncements").classList.toggle("show");
+}
+function HideUpdates()
+{
+  document.getElementById("CourseUpdates").classList.toggle("noshow");
+  document.getElementById("HiddenUpdates").classList.toggle("show");
+}
+function HideLinks()
+{
+  document.getElementById("UserLinks").classList.toggle("noshow");
+  document.getElementById("HiddenUserLinks").classList.toggle("show");
+}
